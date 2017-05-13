338fd1c1c4494c1e700ff71fb11d19a9f11f6b0a
5arif <sarif.onwork@gmail.com>
2017-05-13 16:29:44 +0700

    readme

--- Models/History/RowHistory.cs
+++ Models/History/RowHistory.cs
@@ -4,11 +4,11 @@ namespace AutoLogistic.Models.History
 {
     public class RowHistory
     {
-        public bool     IsDelete    { get; set; }
-        public DateTime CreateDate  { get; set; }
-        public DateTime DeleteDate  { get; set; }
-        public DateTime ModifyDate  { get; set; }
-        public int      ModifyCount { get; set; }
-        public string   ModifyBy    { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+        public int ModifyCount { get; set; } = 0;
+        public string ModifyBy { get; set; }
     }
 }
---
+++ Readme.md
@@ -0,0 +1,5 @@
+Host     : ec2-54-235-248-197.compute-1.amazonaws.com
+Database : d281naldtqvf61
+User     : ffmhfcwegwixam
+Port     : 5432
+Password : 4102dea2d7113a5fff04b624aa9ed30929bcc4fa8a11d8eb9f6971bf8c85e169
